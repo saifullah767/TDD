8e8ffbfa7df4440b3cfa01544efd4aa8cbd32114
unknown <skj99667788@gmail.com>
2022-07-28 01:40:11 +0500

    Fix factorial test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,7 +1,10 @@
+require_relative './../solver'
+
 describe 'Solver' do
   it 'Factorial test' do
+    number = 5
     solver = Solver.new
-    factorial = solver.factorial(n)
-    expect factorial.to eq(0)
+    factorial = solver.factorial(number)
+    expect(factorial).to eq(120)
   end
 end
