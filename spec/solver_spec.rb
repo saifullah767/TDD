f1be0d23f06f460d9c82c4727d55aa217f806e04
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-07-27 23:05:15 +0200

    Add test for fizzbuzz, fizz, buzz and other case

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,17 +1,52 @@
 require_relative './../solver'
 
 describe 'Solver' do
-  it 'Factorial test' do
-    number = 5
-    solver = Solver.new
-    factorial = solver.factorial(number)
-    expect(factorial).to eq(120)
+  describe 'When testing factorial' do
+    it 'Factorial test' do
+      number = 5
+      solver = Solver.new
+      factorial = solver.factorial(number)
+      expect(factorial).to eq(120)
+    end
   end
 
-  it 'reverse word' do
-    world = 'hello'
-    solver = Solver.new
-    reverse = solver.reverse(world)
-    expect(reverse).to eq('olleh')
+  describe 'When testing reverse' do
+    it 'reverse word' do
+      world = 'hello'
+      solver = Solver.new
+      reverse = solver.reverse(world)
+      expect(reverse).to eq('olleh')
+    end
   end
+
+  describe 'When testing fizzbuzz' do
+    it 'fizzbuzz correct' do
+      number = 15
+      solver = Solver.new
+      str_value = solver.fizzbuzz(number)
+      expect(str_value).to eq('fizzbuzz')
+    end
+
+    it 'fizz correct' do
+      number = 3
+      solver = Solver.new
+      str_value = solver.fizzbuzz(number)
+      expect(str_value).to eq('fizz')
+    end
+
+    it 'buzz correct' do
+      number = 5
+      solver = Solver.new
+      str_value = solver.fizzbuzz(number)
+      expect(str_value).to eq('buzz')
+    end
+
+    it 'any other number' do
+      number = 4
+      solver = Solver.new
+      str_value = solver.fizzbuzz(number)
+      expect(str_value).to eq('4')
+    end
+  end
+
 end
